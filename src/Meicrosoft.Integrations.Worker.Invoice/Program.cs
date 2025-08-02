06a00003ee81133093b19d1aabe6bbd13d1b9c26
ebranquinho <branquinhomanu@gmail.com>
2025-08-02 16:26:38 -0300

    chore: add invoice structure

---
+++ src/Meicrosoft.Integrations.Worker.Invoice/Program.cs
@@ -0,0 +1,7 @@
+using Meicrosoft.Integrations.Worker.Invoice;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();

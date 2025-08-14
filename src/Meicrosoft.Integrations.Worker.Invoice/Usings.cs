0e3b00514b13e0a4c1f9fdf6d72b14bdf68cef58
ebranquinho <branquinhomanu@gmail.com>
2025-08-14 20:09:48 -0300

    chore: add invoice worker consumer configuration

---
+++ src/Meicrosoft.Integrations.Worker.Invoice/Usings.cs
@@ -0,0 +1,2 @@
+global using MassTransit;
+global using Meicrosoft.Integrations.Common.IntegrationEvents;
--- src/Meicrosoft.Integrations.Worker.Invoice/Worker.cs
+++ src/Meicrosoft.Integrations.Worker.Invoice/Worker.cs
@@ -1,16 +1,26 @@
 namespace Meicrosoft.Integrations.Worker.Invoice;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker(
+    ILogger<Worker> logger,
+    IPublishEndpoint publishEndpoint) : BackgroundService, IConsumer<OrderCreatedIntegrationEvent>
 {
+    private const string ClassName = nameof(Worker);
+
+    public async Task Consume(ConsumeContext<OrderCreatedIntegrationEvent> context)
+    {
+        logger.LogInformation("{Class} | Starting | OrderId: {OrderId}", ClassName, context.Message.OrderId);
+
+        //TODO update invoice with info
+        //TODO fix total value
+        var invoice = new Domain.OrdersAggregate.Invoice(context.Message.OrderId, 0);
+        //TODO save to database
+        await publishEndpoint.Publish(new InvoicedOrderIntegrationEvent(context.Message.OrderId, invoice));
+
+        logger.LogInformation("{Class} | Finishing | OrderId: {OrderId}", ClassName, context.Message.OrderId);
+    }
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            if (logger.IsEnabled(LogLevel.Information))
-            {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-            await Task.Delay(1000, stoppingToken);
-        }
+        await Task.CompletedTask;
     }
 }
